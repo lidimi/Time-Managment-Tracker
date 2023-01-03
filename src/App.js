e15ddc4a03490f3d7f3bf40af396e92957ac02b4
lidimi <lidsheq97@gmail.com>
2023-01-04 00:20:51 +0100

    basic styles

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 import Aside from "./Aside";
+import Grid from "./Grid";
+import React from "react";
+import { useState } from "react";
 
 export default function App() {
+  const time = "monthly";
+
+  function toggleTimeSpawn() {
+    console.log(":(");
+  }
+
   return (
     <main className="app">
-      <Aside />
+      <Aside toggleTimeSpawn={toggleTimeSpawn} />
+      <Grid time={time} />
     </main>
   );
 }
--- src/Aside.js
+++ src/Aside.js
@@ -1,4 +1,4 @@
-export default function Aside() {
+export default function Aside(props) {
   const avatar = require("./images/image-jeremy.png");
 
   return (
@@ -11,9 +11,9 @@ export default function Aside() {
         <h1>Jeremy Robson</h1>
       </div>
       <section className="timespan">
-        <h2>Daily</h2>
-        <h2>Weekly</h2>
-        <h2>Monthly</h2>
+        <h2 onClick={props.toggleTimeSpawn}>Daily</h2>
+        <h2 onClick={props.toggleTimeSpawn}>Weekly</h2>
+        <h2 onClick={props.toggleTimeSpawn}>Monthly</h2>
       </section>
     </aside>
   );
